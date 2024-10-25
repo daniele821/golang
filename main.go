7d242a9f177d36e1b7cd9073200fd94e6baed25e
Daniele Muffato <danixgithub1@gmail.com>
2024-10-25 06:33:02 +0200

    slices tests

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/fs"
-	"path/filepath"
+	"slices"
 )
 
-func visit(path string, di fs.DirEntry, err error) error {
-	if err != nil {
-		fmt.Println(err)
-		panic("FUUUUUCK")
-	}
-	fmt.Println(path, " | ", di, " | ", err)
-	return nil
-}
-
 func main() {
-	flag.Parse()
-	root := flag.Arg(0)
-	err := filepath.WalkDir(root, visit)
-	fmt.Printf("filepath.WalkDir() returned %v\n", err)
+	slice := []string{"a", "b", "b", "a"}
+	slices.Sort(slice)
+	slice = slices.Compact(slice)
+	fmt.Println(slice)
 }
