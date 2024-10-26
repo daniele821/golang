01e22a5e0e5b259a9a28517ee4928985f40ac46d
Daniele Muffato <danixgithub1@gmail.com>
2024-10-26 22:53:32 +0200

    filecmp2

--- main.go
+++ main.go
@@ -11,16 +11,14 @@ import (
 
 const chunkSize = 64000
 
-func readersEqual(file1, file2 string) bool {
-	r, _ := os.Open(file1)
-	t, _ := os.Open(file2)
+func readersEqual(r io.Reader, t io.Reader) (bool, error) {
 	rBuf := make([]byte, chunkSize)
 	tBuf := make([]byte, chunkSize)
 
 	for {
 		readFromR, errR := r.Read(rBuf)
 		if errR != nil && !errors.Is(errR, io.EOF) {
-			return false
+			return false, errR
 		}
 
 		readFromT := 0
@@ -29,36 +27,35 @@ func readersEqual(file1, file2 string) bool {
 		if readFromR == 0 && errors.Is(errR, io.EOF) {
 			readFromT, errT := t.Read(tBuf[:1])
 			if readFromT == 0 && errors.Is(errT, io.EOF) {
-				return true
+				return true, nil
 			} else {
-				return false
+				return false, errT
 			}
 		}
 
 		for readFromR > readFromT {
 			nextReadFromT, errT := t.Read(tCmpBuf[readFromT:])
 			if errT != nil && !errors.Is(errT, io.EOF) {
-				return false
+				return false, errT
 			}
 			prevReadFromT := readFromT
 			readFromT = prevReadFromT + nextReadFromT
 			if !bytes.Equal(rBuf[prevReadFromT:readFromT], tCmpBuf[prevReadFromT:readFromT]) {
-				return false
+				return false, nil
 			}
 			if errors.Is(errR, io.EOF) && errors.Is(errT, io.EOF) {
-				return true
+				return true, nil
 			}
 			if errors.Is(errR, io.EOF) || errors.Is(errT, io.EOF) {
-				return false
+				return false, nil
 			}
 		}
 	}
 }
-
 func main() {
 	start := time.Now()
-	a := "/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf"
-	b := "/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf"
+	a, _ := os.Open("/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf")
+	b, _ := os.Open("/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf")
 	fmt.Println(readersEqual(a, b))
 	fmt.Println(time.Since(start))
 }
