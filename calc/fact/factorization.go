0e8bb724200b93ab0e21cbf3957c32ef623c8ae1
Daniele Muffato <danixgithub1@gmail.com>
2024-11-09 21:16:58 +0100

    added factorization

--- calc/fact/factorization.go
+++ calc/fact/factorization.go
@@ -1,5 +1,51 @@
 package fact
 
-func factorize(num int64) map[int64]int32 {
-	return nil
+import (
+	"fmt"
+	"math/bits"
+	"strings"
+)
+
+type PrimeFactors map[int]int
+
+func Factorize(num int) PrimeFactors {
+	if num < 2 {
+		return nil
+	}
+	factors := map[int]int{}
+
+	// fast calculate power of 2
+	powTwo := bits.TrailingZeros(uint(num))
+	if powTwo > 0 {
+		num >>= powTwo
+		factors[2] = powTwo
+	}
+
+	// calculate all odd factors
+	// no need to check if divisor is prime:
+	// if divisor isn't prime, its prime factors
+	// are smaller and already included
+	divisor := 3
+	for num > 1 {
+		if num%divisor == 0 {
+			factors[divisor] += 1
+			num /= divisor
+		} else {
+			divisor += 2
+		}
+	}
+
+	return factors
+}
+
+func (p PrimeFactors) String() string {
+	builder := []string{}
+	for key, val := range p {
+		if val == 1 {
+			builder = append(builder, fmt.Sprintf("%d", key))
+		} else {
+			builder = append(builder, fmt.Sprintf("%d^%d", key, val))
+		}
+	}
+	return strings.Join(builder, " * ")
 }
--- main.go
+++ main.go
@@ -1,61 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"errors"
+	"exp/calc/fact"
 	"fmt"
-	"io"
-	"os"
-	"time"
 )
 
-const chunkSize = 64000
-
-func readersEqual(r io.Reader, t io.Reader) (bool, error) {
-	rBuf := make([]byte, chunkSize)
-	tBuf := make([]byte, chunkSize)
-
-	for {
-		readFromR, errR := r.Read(rBuf)
-		if errR != nil && !errors.Is(errR, io.EOF) {
-			return false, errR
-		}
-
-		readFromT := 0
-		tCmpBuf := tBuf[:readFromR]
-
-		if readFromR == 0 && errors.Is(errR, io.EOF) {
-			readFromT, errT := t.Read(tBuf[:1])
-			if readFromT == 0 && errors.Is(errT, io.EOF) {
-				return true, nil
-			} else {
-				return false, errT
-			}
-		}
-
-		for readFromR > readFromT {
-			nextReadFromT, errT := t.Read(tCmpBuf[readFromT:])
-			if errT != nil && !errors.Is(errT, io.EOF) {
-				return false, errT
-			}
-			prevReadFromT := readFromT
-			readFromT = prevReadFromT + nextReadFromT
-			if !bytes.Equal(rBuf[prevReadFromT:readFromT], tCmpBuf[prevReadFromT:readFromT]) {
-				return false, nil
-			}
-			if errors.Is(errR, io.EOF) && errors.Is(errT, io.EOF) {
-				return true, nil
-			}
-			if errors.Is(errR, io.EOF) || errors.Is(errT, io.EOF) {
-				return false, nil
-			}
-		}
-	}
-}
 func main() {
-	start := time.Now()
-	a, _ := os.Open("/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf")
-	b, _ := os.Open("/home/daniele/.local/share/fonts/FiraCode/FiraCodeNerdFont-Bold.ttf")
-	fmt.Println(readersEqual(a, b))
-	fmt.Println(time.Since(start))
+	i := 0xff_ff_ff_ff
+	fmt.Println(i, fact.Factorize(i))
 }
