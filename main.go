10531e8f7d2e5f1a7ac4b6950a9c81704e84df99
Daniele Muffato <danixgithub1@gmail.com>
2024-11-26 09:09:01 +0100

    wip

--- main.go
+++ main.go
@@ -2,36 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
 )
 
-func AllFilesInDir(dir, relPath string) []string {
-	var files []string
-	visit := func(path string, dirEntry fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		fmt.Println(path)
-		if dirEntry.Type().IsRegular() || dirEntry.Type()&os.ModeSymlink == os.ModeSymlink {
-			if relPath != "" {
-				path, err = filepath.Rel(relPath, path)
-				if err != nil {
-					return err
-				}
-			}
-			files = append(files, path)
-		}
-		return nil
-	}
-	err := filepath.WalkDir(dir, visit)
-	if err != nil {
-		return []string{}
-	}
-	return files
-}
-
 func main() {
-	fmt.Println(AllFilesInDir("/home/daniele/.config/kitty", ""))
+	i, err := os.ReadFile("/home/daniele/test")
+	fmt.Println(err)
+	fmt.Println((string(i)))
 }
