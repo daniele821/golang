af7584efd55ce187e974d666881f6eb434c9ce10
Daniele Muffato <danixgithub1@gmail.com>
2024-11-26 06:22:15 +0100

    wip

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"exp/internal/calc"
 	"fmt"
-	"time"
+	"io/fs"
+	"os"
+	"path/filepath"
 )
 
+func AllFilesInDir(dir, relPath string) []string {
+	var files []string
+	visit := func(path string, dirEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		fmt.Println(path)
+		if dirEntry.Type().IsRegular() || dirEntry.Type()&os.ModeSymlink == os.ModeSymlink {
+			if relPath != "" {
+				path, err = filepath.Rel(relPath, path)
+				if err != nil {
+					return err
+				}
+			}
+			files = append(files, path)
+		}
+		return nil
+	}
+	err := filepath.WalkDir(dir, visit)
+	if err != nil {
+		return []string{}
+	}
+	return files
+}
+
 func main() {
-	i := 0x80_00_00_00 + 1
-	start := time.Now()
-	fmt.Println(i, calc.Factorize(i))
-	fmt.Println(time.Since(start))
+	fmt.Println(AllFilesInDir("/home/daniele/.config/kitty", ""))
 }
