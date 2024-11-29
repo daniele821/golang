450b99c117770cb9575895a970a8a791a87fbab9
Daniele Muffato <danixgithub1@gmail.com>
2024-11-29 16:50:35 +0100

    test

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	remote, err := exec.Command("git", "-C", "/personal/repos/daniele821/golang/", "rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}").Output()
 	fmt.Println(string(remote), err)
 	// git rev-parse --abbrev-ref --symbolic-full-name @{u}
+
 }
