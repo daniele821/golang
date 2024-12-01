22c0ad64f32a4d38c33f74cd507464704f342dcc
Daniele Muffato <danixgithub1@gmail.com>
2024-12-01 19:18:58 +0100

    wip

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
@@ -11,8 +10,8 @@ import (
 func main() {
 	file_byte, _ := os.ReadFile("input.txt")
 	file := string(file_byte)
-	arr1 := []string{}
-	arr2 := []string{}
+	arr1 := []int{}
+	arr2 := map[int]int{}
 	for _, line := range strings.Split(file, "\n") {
 		line := strings.TrimSpace(line)
 		if line != "" {
@@ -20,22 +19,20 @@ func main() {
 			if len(splitted) != 2 {
 				panic("wrong amount of elems: ")
 			}
-			arr1 = append(arr1, splitted[0])
-			arr2 = append(arr2, splitted[1])
+			tmp1, _ := strconv.Atoi(splitted[0])
+			arr1 = append(arr1, tmp1)
+			tmp2, _ := strconv.Atoi(splitted[1])
+			if val, ok := arr2[tmp2]; ok {
+				arr2[tmp2] = val + 1
+			} else {
+				arr2[tmp2] = 1
+			}
 		}
 	}
-	slices.Sort(arr1)
-	slices.Sort(arr2)
 	res := 0
-	for index, elem1 := range arr1 {
-		elem2 := arr2[index]
-		elem1_int, _ := strconv.Atoi(elem1)
-		elem2_int, _ := strconv.Atoi(elem2)
-		tmp := elem2_int - elem1_int
-		if tmp < 0 {
-			tmp *= -1
-		}
-		res += tmp
-	}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
 	fmt.Println(res)
 }
