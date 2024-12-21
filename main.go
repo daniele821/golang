90812bd3067a427cd2b430f0b07ee7a99335cdb7
Daniele Muffato <danixgithub1@gmail.com>
2024-12-21 05:51:26 +0100

    lua 1-index iterator

--- main.go
+++ main.go
@@ -2,26 +2,22 @@ package main
 
 import "fmt"
 
-func print(box [3][3]int) {
-	for _, line := range box {
-		for _, cell := range line {
-			fmt.Print(cell, " ")
-		}
-		fmt.Println()
-	}
-}
+var LIMIT int = 5
 
-func oneline(box [3][3]int) [9]int {
-	boxLine := [9]int{}
-	for a, line := range box {
-		for b, cell := range line {
-			boxLine[a*3+b] = cell
-		}
-	}
-	return boxLine
+func iter(i *int, sum int) {
+	*i = (*i+sum+LIMIT-1)%LIMIT + 1
 }
 
 func main() {
-	box := [3][3]int{}
-	print(box)
+	i := 1
+	for range 20 {
+		fmt.Print(i, " ")
+		iter(&i, 3)
+	}
+	fmt.Println()
+	for range 20 {
+		fmt.Print(i, " ")
+		iter(&i, -2)
+	}
+	fmt.Println()
 }
